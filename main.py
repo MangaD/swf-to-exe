491a4a5c6c53681546e690f336cbe7728275e308
MangaD <11088596+MangaD@users.noreply.github.com>
2023-06-07 11:13:52 +0100

    Make file dialog remember the last directory used.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
-from PySide6.QtCore import Slot, QFile, QIODevice, QTextStream
+from PySide6.QtCore import (
+    Slot, QFile, QIODevice,
+    QTextStream, QCoreApplication,
+    QSettings, QDir
+)
 
 from forms.ui_form import Ui_MainWindow
 
@@ -17,6 +21,14 @@ class MainWindow(QMainWindow):
         self.ui.addSwfBtn.clicked.connect(self.addSwfFile)
         self.setTheme()
 
+
+    def setTheme(self):
+        styleFile = QFile(u":/theme/resources/PicPax.qss")
+        if styleFile.open(QIODevice.ReadOnly | QIODevice.Text):
+            inStream = QTextStream(styleFile)
+            _style = inStream.readAll()
+            self.setStyleSheet(_style)
+
     @Slot()
     def about(self):
         QMessageBox.about(self, 'About Swf2Exe', 'Convert your SWF files to EXE files using the Adobe Flash Player Projector.')
@@ -26,35 +38,33 @@ class MainWindow(QMainWindow):
         fileList = self.openFileNamesDialog()
         self.ui.swfList.addItems(fileList)
 
-    def setTheme(self):
-        styleFile = QFile(u":/theme/resources/PicPax.qss")
-        if styleFile.open(QIODevice.ReadOnly | QIODevice.Text):
-            inStream = QTextStream(styleFile)
-            _style = inStream.readAll()
-            self.setStyleSheet(_style)
-
-    def openFileNameDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","Shockwave Flash (*.swf);;All Files (*)", options=options)
-        if fileName:
-            print(fileName)
-
     def openFileNamesDialog(self):
+        DEFAULT_DIR_KEY = "default_dir"
+        settings = QSettings()
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        files, _ = QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()", "","Shockwave Flash (*.swf);;All Files (*)", options=options)
+        files, _ = QFileDialog.getOpenFileNames(self,
+            "Select file(s)",
+            settings.value(DEFAULT_DIR_KEY),
+            "Shockwave Flash (*.swf);;All Files (*)",
+            options=options)
+        if files:
+            currentDir = QDir()
+            settings.setValue(DEFAULT_DIR_KEY, currentDir.absoluteFilePath(files[-1]));
         return files
 
-    def saveFileDialog(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","EXE Files (*.txt)", options=options)
-        if fileName:
-            print(fileName)
+# Set up info for using with QSettings
+# https://stackoverflow.com/a/3598245/3049315
+# https://srinikom.github.io/pyside-docs/PySide/QtCore/QSettings.html
+def setAppInfo() -> None:
+    QCoreApplication.setApplicationName("Swf2Exe")
+    QCoreApplication.setApplicationVersion("1.0.0.0")
+    QCoreApplication.setOrganizationName("swf2exe")
+    QCoreApplication.setOrganizationDomain("mangad.github.io/swf2exe")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    setAppInfo()
     widget = MainWindow()
     widget.show()
     sys.exit(app.exec())
